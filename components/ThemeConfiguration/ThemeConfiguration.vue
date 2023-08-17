da5c95508b41e7c7283efc5dd5f9d063030115d3
Simon Erslev Milfred <smi@limbo.works>
2023-08-17 12:49:33 +0200

    fix: removed duplicate

--- components/ThemeConfiguration/ThemeConfiguration.vue
+++ components/ThemeConfiguration/ThemeConfiguration.vue
@@ -29,25 +29,6 @@ const observedData = ref({
 	config: {},
 });
 
-const isObject = (item) => (item && typeof item === 'object' && !Array.isArray(item));
-const deepmerge = (target, ...sources) => {
-	if (!sources.length) return target;
-	const source = sources.shift();
-
-	if (isObject(target) && isObject(source)) {
-		for (const key in source) {
-			if (isObject(source[key])) {
-				if (!target[key]) Object.assign(target, { [key]: {} });
-				deepmerge(target[key], source[key]);
-			} else {
-				Object.assign(target, { [key]: source[key] });
-			}
-		}
-	}
-
-	return deepmerge(target, ...sources);
-};
-
 export const { config } = observedData.value;
 export default defineNuxtComponent({
 	name: 'ThemeConfiguration',
