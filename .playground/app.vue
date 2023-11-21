d1a919042fbbfc6b4303e7e5f685117baddd61b7
Thomas Fjordside <tfjordside@skybrud.dk>
2023-11-21 09:57:32 +0100

    fix: border & text color bug

--- .playground/app.vue
+++ .playground/app.vue
@@ -1,10 +1,14 @@
 <template>
 	<div class="bg-fishStick bg-scope-fishStick">
-		<ThemeConfiguration use-theme-classes> Yes it is loaded </ThemeConfiguration>
+		<ThemeConfiguration use-theme-classes>
+			Yes it is loaded
+		</ThemeConfiguration>
 		<div class="text-strong">Test?</div>
 	</div>
-	<div class="u-theme-default bg-primary">
-		<div class="text-strong">Test?</div>
+	<div class="bg-primary">
+		<div class="u-theme-default bg-primary border-3 border-danger">
+			<div class="text-onSurfaceDanger">Test?</div>
+		</div>
 	</div>
 </template>
 
